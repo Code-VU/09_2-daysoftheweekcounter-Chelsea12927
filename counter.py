225e38f8d58512967707375292a3978abaea644e
Chelsea Hamilton <chels@Chelseas-iMac.local>
2023-03-30 21:33:04 -0500

    count day of week emails sent

--- counter.py
+++ counter.py
@@ -2,6 +2,17 @@ def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
 
+    daycount = dict()
+
+    with open(file_name) as file:
+        for line in file:
+            if line.startswith("From "):
+                linebrokenout = line.split()
+                day = linebrokenout[2]
+                daycount[day] = daycount.get(day,0) + 1
+        print(daycount)
+
+
 
 
 ## if you want to test locally run > python payCalculator.py
